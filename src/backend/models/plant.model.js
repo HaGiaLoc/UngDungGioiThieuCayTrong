afb4ee921806a9d0763e8eed6de7d08019c211a4
Dogiahao23 <xanhlhdhok@gmail.com>
2025-07-22 16:56:39 +0700

    Thiết kế bảng trong MySQL GTCT-20 #done

---
+++ src/backend/models/plant.model.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Plant = sequelize.define('Plant', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: 'Hiển thị',
+  },
+  featured: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+}, {
+  tableName: 'plants',
+  timestamps: true,
+});
+
+module.exports = Plant; 
