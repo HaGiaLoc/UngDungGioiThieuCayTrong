fe0c261d03fbd6432c8640665c3a087f97f411e8
KhangLa <lathuankhang2004@gmail.com>
2025-07-22 15:07:21 +0700

    Tích hợp Swagger GTCT-28 #done

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const plantRoutes = require('./routes/plant.routes');
+const swaggerUi = require(require.resolve('swagger-ui-express'));
+const swaggerJSDoc = require(require.resolve('swagger-jsdoc'));
 const cors = require('cors');
 const { connectDB } = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Green Garden API',
+    version: '1.0.0',
+    description: 'Tài liệu RESTful API cho Green Garden',
+  },
+  servers: [
+    { url: 'http://localhost:3000', description: 'Local server' },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 // Khi truy cập root, tự động chuyển hướng sang Swagger UI
 app.get('/', (req, res) => {
@@ -14,6 +35,7 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(cors());
 app.use('/api/plants', plantRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 connectDB().then(() => {
   app.listen(PORT, () => {
